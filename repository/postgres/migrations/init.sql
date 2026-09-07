c14ce7cbc9a1c2b41a7540f68712bc297521b059
Kirill Sipkin <ssdhddq@vk.com>
2026-09-07 15:14:34 +0700

    add SQL migrations

---
+++ repository/postgres/migrations/init.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXIST users (
+    id UUID PRIMARY KEY,
+    login TEXT UNIQUE NOT NULL,
+    password_hash TEXT NOT NULL
+);
+
+CREATE TABLE IF NOT EXIST sessions (
+    session_id UUID PRIMARY KEY,
+    user_id UUID REFERENCES users(id) ON DELETE CASCADE,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL
+);
+
+CREATE TABLE IF NOT EXIST tasks (
+    id UUID PRIMARY KEY,
+    translator TEXT NOT NULL,
+    code TEXT NOT NULL,
+    status TEXT NOT NULL DEFAULT 'default',
+);
+
+CREATE INDEX idx_session_user_id ON sessions(user_id);
